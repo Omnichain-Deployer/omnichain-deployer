6b4afe8c37bfbdb5169dae58c04dac90c466f695
0xchandra <143407433+0xchandra@users.noreply.github.com>
2024-07-06 04:20:01 +0530

    update deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,35 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  console.log("Omnichain Deployer");
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime =  1723880951;
+
+  const feeData = await hre.ethers.provider.getFeeData();
+
+  const lockedAmount = hre.ethers.parseEther("0.000001");
+
+  //console.log(hre.network);
+
+  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {gasPrice: feeData.maxFeePerGas});
+
+  console.log(
+    `Lock with ${hre.ethers.formatEther(
+      lockedAmount
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.log("Error");
+  console.error(error);
+  process.exitCode = 1;
+});
